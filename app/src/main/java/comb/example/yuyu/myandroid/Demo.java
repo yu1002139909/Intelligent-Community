90892ee1f92fb085ada6af53c325931a4ed9cee7
yu <1002139909@qq.com>
2017-05-06 11:26:08 +0800

    初始化项目

---
+++ app/src/main/java/comb/example/yuyu/myandroid/Demo.java
@@ -0,0 +1,14 @@
+package comb.example.yuyu.myandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yuyu on 2017/5/6.
+ */
+public class Demo  extends Activity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+ }
